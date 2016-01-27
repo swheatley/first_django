7c278fd21113c41a0df753a37e1e1cf5679887b0
swheatley <shaylee1@me.com>
2016-01-27 03:31:02 -0700

    Charles version of scrape.py4

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,103 +1,61 @@
 #!/usr/bin/env python
 import urllib
 import urllib2
-from lxml import etree
 import StringIO
 import re, sys, os
-# import request
-from PIL import Image
 
-
+from lxml import etree
 
 sys.path.append("..")
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
-from main.models import State, StateCapital
+from main.models import State
 from django.core.files import File
 from django.core.files.temp import NamedTemporaryFile
-#result = urllib.urlopen("http://164.100.47.132/LssNew/Members/Alphabaticallist.aspx")
 
 result = urllib.urlopen("http://www.50states.com/")
-
 html = result.read()
 
 parser = etree.HTMLParser()
 tree   = etree.parse(StringIO.StringIO(html), parser)
 
-# xpath = "//table[@id='ctl00_ContPlaceHolderMain_Alphabaticallist1_dg1']/tr/td/img/@src"  #/child::text()
+href_xpath = "//*[@id='ar-full-homepage']/div/ul/li/a/@href"
 
-# xpath1 = "//table[@id='Table2']/tr/td/a/child::text()"
+filtered_html = tree.xpath(href_xpath)
 
-# xpath2 = "////table[@id='Table2']/tr/td/a/@href"
-
-xpath3 = "//*[@id='ar-full-homepage']/div/ul/li/a/@href"
-
-
-filtered_html3 = tree.xpath(xpath3)
-
-
-links = [html for html in filtered_html3 if 'htm' in html]
-
-print links
+links = [html for html in filtered_html if 'htm' in html]
 
 for link in links:
-    state_name_pattern = "(?<=\W)[^.]*(?=.htm)"
-
+    state_name_pattern = "(?<=\W).*(?=.htm)"
     state_name_search = re.search(state_name_pattern, link)
-
     state_name = "%s" % state_name_search.group()
 
-
-    print "#### %s #####" % state_name.strip('new')
-
-
-    try:
-        state_object, created = State.objects.get_or_create(name__icontains=state_name.strip('new'))
-    except:
-        state_object = State.objects.filter(name__icontains=state_name.strip('new')).first()
-
-    print created
-
-    #print "http://www.50states.com/%s" % link
     state_page = urllib.urlopen("http://www.50states.com%s" % link)
-
     state_page_html = state_page.read()
-
-    tree   = etree.parse(StringIO.StringIO(state_page_html), parser)
+    tree = etree.parse(StringIO.StringIO(state_page_html), parser)
 
     state_population_xpath = '//*[@id="collapseQuick-Facts"]/div/ul/li[6]/div/text()'
-
     state_population_string = tree.xpath(state_population_xpath)
-    state_population_pattern = "([\d]+),([\d]+),([\d]+)"
+
+    state_population_pattern = "\d+,\d+,\d+"
     cleaned_pop_string = re.search(state_population_pattern, "%s" % state_population_string)
 
     try:
-        print cleaned_pop_string.group()
-        state_object.population = "%s" % cleaned_pop_string.group()
-        try:
-            state_object.save()
-        except:
-            print state_object.name
-    except AttributeError:
-        print "no groups"
+        state_object, created = State.objects.get_or_create(name__icontains=state_name.strip('new'))
+        state_object.population = cleaned_pop_string.group()
+    except Exception, e:
+        print e
+        print "ERROR AT %s" % state_name
 
     state_map_link_xpath = '//*[@id="collapseGovernment"]/div/ul/li[2]/div/a/@href'
     state_map_link = tree.xpath(state_map_link_xpath)
 
-
     state_page = urllib.urlopen(state_map_link[0])
-
     state_page_html = state_page.read()
-
     tree = etree.parse(StringIO.StringIO(state_page_html), parser)
 
-    state_map_image_link = '/html/body/img/@src'
-
-    state_map_image = tree.xpath(state_map_image_link)
-
-
-    print "STATE MAP IMAGE LINK %s" % 'http://quickfacts.census.gov%s' % state_map_image[0]
+    state_map_image_xpath = '/html/body/img/@src'
+    state_map_image = tree.xpath(state_map_image_xpath)
 
     url = 'http://quickfacts.census.gov%s' % state_map_image[0]
     image_response = urllib2.urlopen(url).read()
@@ -107,38 +65,6 @@ for link in links:
     img_temp.write(image_response)
 
     state_object.state_map.save('tmpimage.gif', File(img_temp))
-
-        # img = StringIO.StringIO(image_response)
-        #img = Image.open(StringIO.StringIO(image_response.content))
-
-        # image_file = open('temp.gif', 'wb')
-
-        # for block in image_response.iter_content(1024):
-        #   image_file.write(block)
-
-
-
-    # image = urllib.URLopener()
-    
-    #   f = open(StringIO.StringIO(), 'wb')
-    #     f.write(chunk)
-    
-    #    = f
-    #   state_object.save()
-
-# ([\d{0,2}])(,\d{0,3})
-
-# ^([0-9]+,)*[0-9]+$
-
-
-
-# state_page_result = state_page.read()
-# for html in filtered_html3:
-#   # if '.html' in html:
-#   print html
-
-
-# print filtered_html3
 # !/usr/bin/env python
 
 # # import urllib
